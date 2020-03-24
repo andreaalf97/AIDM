c669363028152175eacb55fe58d4808359386bc5
tpapak01 <tpapak01@cs.ucy.ac.cy>
2020-03-25 00:24:11 +0100

    Added a few touches

--- part3/IDM/src/Homework.java
+++ part3/IDM/src/Homework.java
@@ -30,14 +30,14 @@ public class Homework {
 	}
 	
 	// Solve unconstrained problem for 1 agent with value iteration
-	public static void task1() {		
+	public static void task1() {
 		// Get CMDP model for 1 agent
 		CMDP cmdp = UserGenerator.getCMDPChild();
 		CMDP[] cmdps = new CMDP[]{cmdp};
 		
 		// Solve the problem without constraints
 		PlanningAlgorithm alg = new PlanningAlgorithm();		
-		Solution solution = alg.solveVI(cmdps, 0.95);
+		Solution solution = alg.solveVI(cmdps);
 		System.out.println("Expected reward: "+solution.getExpectedReward());
 		System.out.println("Expected cost: "+solution.getExpectedCost());
 		
@@ -49,6 +49,7 @@ public class Homework {
 		// Print policy of agent 0
 		int agentID = 0;
 		double[][] policy = solution.getPolicy(agentID);
+
 		System.out.println("\nPOLICY");
 		System.out.println("--------------------------------");
 		for(int s=0; s<cmdps[agentID].getNumStates(); s++) {
@@ -59,6 +60,8 @@ public class Homework {
 			}
 			System.out.println();
 		}
+
+
 		
 	}
 	
@@ -174,6 +177,6 @@ public class Homework {
 	}
 	
 	public static void main(String[] args) {
-		task1();
+			task1();
 	}
 }
